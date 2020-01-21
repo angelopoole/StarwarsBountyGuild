e1650a7faf154ba8c42d7ca835d05658d67e9f0b
angelopoole <angelo.poole@outlook.com>
2020-01-21 16:58:43 -0500

    gonna pull changes

--- app/controllers/hunter_contracts_controller.rb
+++ app/controllers/hunter_contracts_controller.rb
@@ -1,16 +1,26 @@
 class HunterContractsController < ApplicationController
-    before_action :strong_params, only [:create, :update, :delete]
+    before_action :find_hunter_contract, only [:create, :update, :delete]
 
     def new 
-        @hunter = Hunter.new
+        @huntercontract = HunterContract.new
     end
 
     def create
-        @hunter = Hunter.create
-        redirect_to hunter_path(@hunter)
+        @huntercontract = HunterContract.create
+        redirect_to contract_path(@huntercontract.contract)
+    end
+
+    def show
+        # @hunter = Hunter.find(params[:id])
+    find_hunter
+    end
     private
 
-    def strong_params
+    def find_hunter_contract
+        @hunter = Hunter.find(params[:id])
+    end
+
+    def hunter_contract_params
         params.require(:hunter).permit(:name)
     end
 end
--- app/controllers/hunters_controller.rb
+++ app/controllers/hunters_controller.rb
@@ -1,2 +1,12 @@
 class HuntersController < ApplicationController
+    before_action :find_hunter, only: [:show, :edit, :update, :destroy]
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+
 end
